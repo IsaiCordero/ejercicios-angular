17a158e464aff189f762823a90598cbd3b5ef4d3
isai <isaicordero03@gmail.com>
2026-05-11 20:29:08 +0100

    Nuevas cosas y demas

--- angular-nivel-medio/src/app/hijo/hijo.ts
+++ angular-nivel-medio/src/app/hijo/hijo.ts
@@ -7,10 +7,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './hijo.css',
 })
 export class Hijo {
-  @Input() Lista: string[] | undefined;
+  @Input() Lista: { id: number; nombre: string; edad: number }[] = [];
   @Output() eventoEliminar = new EventEmitter<number>();
 
   eliminarUsuario(indice: number){
     this.eventoEliminar.emit(indice);
+  }
+
+  @Input() Texto: string | undefined;
+  @Input() Tareas: {id: number; tarea: string; completada: boolean}[] = [];
+  @Output() eventoCambiar = new EventEmitter<{id: number; tarea: string; completada: boolean}>();
+  completarTarea(tarea: {id: number; tarea: string; completada: boolean}){
+    this.eventoCambiar.emit(tarea);
   }
 }
--- angular-nivel-medio/src/app/padre/padre.ts
+++ angular-nivel-medio/src/app/padre/padre.ts
@@ -9,10 +9,23 @@ import { Hijo } from '../hijo/hijo';
 })
 export class Padre {
   textoPadre = "Hola a todos soy Ibai Llanos"
-  Lista = ["CarlosFc02", "WIllyrex", "ibaiLLanos", "Oregano12"];
+  Lista = [
+    {id: 1, nombre: "Carlos", edad: 2},
+    {id: 2, nombre: "Ibai LLanos", edad: 20},
+    {id: 3, nombre: "Messi", edad: 19},
+  ];
   indice: number | undefined;
 
   eliminar($event: number){
     this.Lista.splice($event, 1);
+  }
+
+  Tareas = [
+    {id: 1, tarea: "ver Shrek 2", completada: false},
+    {id: 2, tarea: "volver a ver Shrek 2 pero en ingles", completada: false}
+  ];
+
+  changeState(tarea: { id: number; tarea: string; completada: boolean }){
+    tarea.completada = !tarea.completada;
   }
 }
